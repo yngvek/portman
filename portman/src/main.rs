f6943ea0b8481bd2b58e7be558da59979c6a243d
yngvek <yngve@klakegg.org>
2019-01-15 21:14:37 +0100

    init

---
+++ portman/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    fn main() {
+    let amountSatoshis = 100000000;
+    let amountUSD = 5000;
+    let wantedPercentageBitcoin = 50;
+    let wantetPercentageUSD = 50;
+    let percentageRebalance = 5;
+
+    
+    println!("Hello, world!");
+}
+
+}
